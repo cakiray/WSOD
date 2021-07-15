7a995523e94f8be0310a6f6c79d8d615f630bbfd
ge75hix <ezgi.cakir@tum.de>
2021-07-15 12:34:37 +0200

    merging prec&recall implementation

--- spvnas/core/modules/peak_stimulator.py
+++ spvnas/core/modules/peak_stimulator.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import torch.nn.functional as F
 from torch import autograd
-import spvnas.core.utils as utils
+import core.utils as utils
 
 class PeakStimulation(autograd.Function):
     @staticmethod
@@ -91,7 +91,7 @@ def prm_backpropagation(inputs, outputs, peak_list, peak_threshold=0.08, normali
                 prm[prm==float('inf')] = 0.0
                 prm[prm==float('-inf')] = 0.0
 
-            prm = utils.segment_ground(points=inputs, preds=prm, distance_threshold=0.15)
+            prm = utils.segment_ground(points=inputs.F, preds=prm, distance_threshold=0.15)
             #prm = grad.sum(1).clone().clamp(min=0).detach().cpu()
             #prm = prm.sum(1) # sums columns
             #peak_response_maps.append( prm / prm.sum() )
