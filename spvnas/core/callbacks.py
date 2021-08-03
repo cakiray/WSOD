3872f262d5f70b0dbbdd410f0e98bf09154bba67
ezgic <ezgicakir96@hotmail.com>
2021-08-03 11:34:24 +0200

    deleting self.size in callbacks

--- spvnas/core/callbacks.py
+++ spvnas/core/callbacks.py
@@ -27,7 +27,7 @@ class Shrinkage(Callback):
 
 
     def _before_epoch(self):
-        self.size = 0
+        #self.size = 0
         self.errors = 0
 
     def _after_step(self, output_dict: Dict[str, Any]) -> None:
@@ -40,14 +40,15 @@ class Shrinkage(Callback):
         deniminator = 1 + torch.exp(self.a* (self.c-l_mean))
         error = l2/deniminator
 
-        self.size += targets.size(0)
-        self.errors += error.item() * targets.size(0)
+        #self.size += targets.size(0)
+        #self.errors += error.item() * targets.size(0)
+        self.errors += error.item()
 
     def _after_epoch(self) -> None:
-        self.size = dist.allreduce(self.size, reduction='sum')
-        self.errors = dist.allreduce(self.errors, reduction='sum')
+        #self.size = dist.allreduce(self.size, reduction='sum')
+        self.errors = dist.allreduce(self.errors, reduction='mean')
         if hasattr(self, 'trainer') and hasattr(self.trainer, 'summary'):
-            self.trainer.summary.add_scalar(self.name, self.errors / self.size)
+            self.trainer.summary.add_scalar(self.name, self.errors )
 
 class MSE(Callback):
     def __init__(self,
@@ -101,7 +102,7 @@ class MTE(Callback):
     def _after_epoch(self) -> None:
         self.errors = dist.allreduce(self.errors, reduction='mean')
         if hasattr(self, 'trainer') and hasattr(self.trainer, 'summary'):
-            self.trainer.summary.add_scalar(self.name, self.errors / self.size)
+            self.trainer.summary.add_scalar(self.name, self.errors)
 
 
 class MeanIoU(Callback):
