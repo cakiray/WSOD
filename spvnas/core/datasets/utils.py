18796e6bdd39c2504ab83e11eed009f042a03bd4
ezgic <ezgicakir96@hotmail.com>
2021-08-07 11:26:45 +0200

    cirriculum training is implemented.

---
+++ spvnas/core/datasets/utils.py
@@ -0,0 +1,120 @@
+import os
+import numpy as np
+
+
+
+def read_labels( label_path):
+    label = np.loadtxt(label_path,
+                       dtype={'names': ('type', 'truncated', 'occuluded', 'alpha', 'xmin', 'ymin', 'xmax', 'ymax', 'h', 'w', 'l', 'x', 'y', 'z','rotation_y'),
+                              'formats': ('S14', 'float', 'float', 'float', 'float', 'float', 'float', 'float','float', 'float', 'float', 'float', 'float', 'float', 'float')})
+
+    if label.size == 1:
+        label = label[np.newaxis]
+
+    return label
+
+def read_points( lidar_path, idx):
+    file = open(lidar_path, 'rb')
+    points = np.fromfile(file, dtype=np.float32).reshape(-1, 4)[:,0:3]  # .astype(np.float16)
+    return points
+
+def normalize(arr):
+    min = np.min(arr[arr>0.0])
+    max = np.max(arr)
+    arr = (arr-min)/(max-min)
+    arr[arr<0.0] = 0.0
+    return arr
+
+def distance( p1, p2, _in3d=False):
+    if _in3d:
+        return np.sqrt( (p1[:,0]-p2[0])**2 +  (p1[:,1]-p2[1])**2 + (p1[:,2]-p2[2])**2 ).astype('float')
+    else:
+        dist =  np.sqrt( (p1[:,0]-p2[0])**2 + (p1[:,1]-p2[1])**2 ).astype('float')
+        return dist
+
+# calculates the normalized value of object in point cloud
+def set_normal_label(max, min, point, filt , _in3d=False):
+    labels =  1 - ( distance(point[:,0:3] , min, _in3d) / distance(np.array([max]),min, _in3d) )
+    return labels[filt]
+
+
+def get_distance(points, center, _in3d = False):
+    labels =  distance(points[:,0:3] , center, _in3d) # calculate distance of points to center point
+    #labels[labels>=threshold] = 0 # discard the points that are away than threshold, meaning no longer belong to a object
+    return labels
+
+def standardize(data, threshold):
+    labels_normalized = data / threshold # normalize labels
+    return labels_normalized
+
+def substact_1(data):
+    data = 1- data # make center to be 1 and values decrease while getting away
+    data[data<0] = 0
+    return data.reshape(-1,1)
+
+
+def project_ref_to_velo(pts_3d_ref, C2V):
+    pts_3d_ref = cart2hom(pts_3d_ref) # nx4
+    return np.dot(pts_3d_ref, np.transpose(C2V))
+
+def project_rect_to_ref( pts_3d_rect, R0):
+    ''' Input and Output are nx3 points '''
+    return np.transpose(np.dot(np.linalg.inv(R0), np.transpose(pts_3d_rect)))
+
+def project_rect_to_velo(calibs, pts_3d_rect):
+    ''' Input: nx3 points in rect camera coord.
+                     This part is what I want, I need to convert 3D coordinates to lidar coordinates
+        Output: nx3 points in velodyne coord.
+    '''
+    pts_3d_ref = project_rect_to_ref(pts_3d_rect, calibs['R0'])
+    return project_ref_to_velo(pts_3d_ref, calibs['C2V'])
+
+def cart2hom( pts_3d):
+    ''' Input: nx3 points in Cartesian Cartesian is to add a column 1
+        Oupput: nx4 points in Homogeneous by pending 1
+    '''
+    n = pts_3d.shape[0]
+    pts_3d_hom = np.hstack((pts_3d, np.ones((n,1))))
+
+    return pts_3d_hom
+
+def read_calibs(calibs_path):
+    calibs = {}
+    with open(calibs_path, 'r') as f:
+        for line in f.readlines():
+            line = line.rstrip()
+            if len(line)==0: continue
+            key, value = line.split(':', 1)
+            # The only non-float values in these files are dates, which
+            # we don't care about anyway
+            try:
+                calibs[key] = np.array([float(x) for x in value.split()])
+            except ValueError:
+                pass
+    # Projection matrix from rect camera coord to image2 coord
+    P = calibs['P2']
+    P = np.reshape(P, [3,4])
+    calibs['P2'] = P
+    # Rigid transform from Velodyne coord to reference camera coord
+    V2C = calibs['Tr_velo_to_cam']
+    V2C = np.reshape(V2C, [3,4])
+    calibs['V2C'] = V2C
+
+    C2V = inverse_rigid_trans(V2C)
+    calibs['C2V'] = C2V
+    # Rotation from reference camera coord to rect camera coord
+    R0 = calibs['R0_rect']
+    R0 = np.reshape(R0,[3,3])
+    calibs['R0'] = R0
+
+    return calibs
+
+
+def inverse_rigid_trans(Tr):
+    ''' Inverse a rigid body transform matrix (3x4 as [R|t])
+        [R'|-R't; 0|1]
+    '''
+    inv_Tr = np.zeros_like(Tr) # 3x4
+    inv_Tr[0:3,0:3] = np.transpose(Tr[0:3,0:3])
+    inv_Tr[0:3,3] = np.dot(-np.transpose(Tr[0:3,0:3]), Tr[0:3,3])
+    return inv_Tr
