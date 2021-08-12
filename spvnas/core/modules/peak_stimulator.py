685211509fc7354caca6e4401b77cfe842ab3982
ezgic <ezgicakir96@hotmail.com>
2021-08-12 21:43:45 +0200

    thresholding is removed from peak backprop.

--- spvnas/core/modules/peak_stimulator.py
+++ spvnas/core/modules/peak_stimulator.py
@@ -2,7 +2,6 @@ import torch
 import numpy as np
 import torch.nn.functional as F
 from torch import autograd
-import core.utils as utils
 
 class PeakStimulation(autograd.Function):
     @staticmethod
@@ -54,47 +53,39 @@ def prm_backpropagation(inputs, outputs, peak_list, peak_threshold=0.08, normali
     # PRM paper to calculate gradient
     grad_output = outputs.new_empty(outputs.size())
     grad_output.zero_()
-    prm = None
     #print("max and min values in PRM: ", torch.max(outputs), torch.min(outputs))
     
     valid_peak_list = []
     peak_response_maps = []
     peak_response_maps_con = np.zeros((inputs.F.shape))
-    i = 0
-    #print("max min in output ", torch.max(outputs), torch.min(outputs))
+
     for idx in range(peak_list.size(0)):
         peak_val = outputs[peak_list[idx, 0], peak_list[idx, 1], peak_list[idx, 2]]
         
         if peak_val > peak_threshold:
-            #print("PEAK VALL ", peak_val)
             grad_output.zero_()
             # Set 1 to the max of predicted center points in gradient
             grad_output[peak_list[idx, 0], peak_list[idx, 1], peak_list[idx, 2]] = 1
 
             if inputs.F.grad is not None:
-                inputs.F.grad.zero_() # shape is Nx4 , 2D
+                inputs.F.grad.zero_() # shape is N x input_channel_num , 2D
 
             # Calculate peak response maps backwarding the output
             outputs.backward(grad_output, retain_graph=True)
-            #print("sè¦ºfè¦ºrr:", grad_output[torch.argmax(outputs, dim=0)] )
 
-            grad = inputs.F.grad # Nx4
-            #grad = torch.sum(grad[:,0:2],1)
+            grad = inputs.F.grad # N x input_channel_num
             # PRM is absolute of all channels
             prm = grad.detach().cpu().clone()
-            prm = np.absolute( prm ) # shape: Nx4, 2D
-            # 0 <= prm <= 1
+            prm = np.absolute( prm ) # shape: N x input_channel_num, 2D
+
+            #normalize gradient 0 <= prm <= 1
             if normalize:
-                #normalize gradient
                 mins= np.amin(np.array(prm[prm>0.0]), axis=0)
                 maxs = np.amax(np.array(prm), axis=0)
                 prm = (prm-mins)/(maxs-mins)
                 prm[prm==float('inf')] = 0.0
                 prm[prm==float('-inf')] = 0.0
-                prm[prm<0.005] = 0.0 # some thresholding
 
-            #segment ground to reduce false positives
-            prm = utils.segment_ground(points=inputs.F, preds=prm, distance_threshold=0.15)
             #prm = grad.sum(1).clone().clamp(min=0).detach().cpu()
             #prm = prm.sum(1) # sums columns
             #peak_response_maps.append( prm / prm.sum() )
@@ -103,9 +94,7 @@ def prm_backpropagation(inputs, outputs, peak_list, peak_threshold=0.08, normali
             peak_response_maps_con +=np.asarray( prm)
             #valid_peak_list contains indexes of valid peaks in center response map, shape: Mx3, e.g.[0,0,idx]
             valid_peak_list.append(peak_list[idx,:])
-            i += 1
 
-    #print("i ", i)
     if len(peak_response_maps) >0:
         # shape = len(valid_peak_list), 2
         valid_peak_list = torch.stack(valid_peak_list) # [1,1,N] -> dimension of each channels of it
