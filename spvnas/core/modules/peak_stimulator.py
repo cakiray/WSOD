2a2a719b7975ea1c798e9a4e1d66c16a9f8e84d9
ezgic <ezgicakir96@hotmail.com>
2021-09-15 16:28:06 +0300

    code cleaned

--- spvnas/core/modules/peak_stimulator.py
+++ spvnas/core/modules/peak_stimulator.py
@@ -82,74 +82,51 @@ def prm_backpropagation(inputs, outputs, peak_list, peak_threshold=0.08, normali
 
     #for idx in range(peak_list.size(0)):
     for idx in range(len(valid_peak_list)):
-        #peak_val = outputs[peak_list[idx, 0], peak_list[idx, 1], peak_list[idx, 2]]
-        #if peak_val > peak_threshold:
-        if True:
-            grad_output.zero_()
-            # Set 1 to the max of predicted center points in gradient
-            #grad_output[peak_list[idx, 0], peak_list[idx, 1], peak_list[idx, 2]] = 1e10
+        grad_output.zero_()
+        # Set 1 to the max of predicted center points in gradient
+        #grad_output[peak_list[idx, 0], peak_list[idx, 1], peak_list[idx, 2]] = 1e10
 
-            # Set K nearest neighbors of peak as 1, backpropagate from a group of points
-            k=1
-            #knn_list = utils.KNN(points=inputs.F, anchor=peak_list[idx,2], k=k)
-            knn_list = utils.KNN(points=inputs.F, anchor=valid_peak_list[idx][2], k=k)
-            for n in knn_list:
-                grad_output[valid_peak_list[idx][0], valid_peak_list[idx][1], n] = 1
-                #grad_output[peak_list[idx, 0], peak_list[idx, 1], n] = 1
+        # Set K nearest neighbors of peak as 1, backpropagate from a group of points
+        k=1
+        #knn_list = utils.KNN(points=inputs.F, anchor=peak_list[idx,2], k=k)
+        knn_list = utils.KNN(points=inputs.F, anchor=valid_peak_list[idx][2], k=k)
+        for n in knn_list:
+            grad_output[valid_peak_list[idx][0], valid_peak_list[idx][1], n] = 1
+            #grad_output[peak_list[idx, 0], peak_list[idx, 1], n] = 1
 
-            if inputs.F.grad is not None:
-                inputs.F.grad.zero_() # shape is N x input_channel_num , 2D
+        if inputs.F.grad is not None:
+            inputs.F.grad.zero_() # shape is N x input_channel_num , 2D
 
-            # Calculate peak response maps backwarding the output
-            outputs.backward(grad_output, retain_graph=True)
-        
-            grad = inputs.F.grad # N x input_channel_num
-            
-            # PRM is absolute of all channels
-            prm = grad.detach().cpu().clone()
-            prm = np.asarray(np.absolute( prm )) # shape: N x input_channel_num, 2D
-            #normalize gradient 0 <= prm <= 1
-            if normalize:
-                prm = utils.maxpool(prm) #channel no is 1 from now on
-                #mins= np.amin(np.array(prm[prm>0.0]), axis=0)
-                #print(prm[prm>0.0].shape)
-                mins = np.asarray( [ np.amin(prm[prm[:,i]>0.0][:,i]) for i in range(prm.shape[1]) ] )
-                maxs = np.amax(np.array(prm), axis=0)
-                prm = (prm-mins)/(maxs-mins)
-                #print("min max ", mins, maxs)
-                prm[prm==float('inf')] = 0.0
-                prm[prm==float('-inf')] = 0.0
-                avg_sum += np.mean(prm[prm>0.0])
-                prm[prm<0.001] = 0.0
+        # Calculate peak response maps backwarding the output
+        outputs.backward(grad_output, retain_graph=True)
 
-                #prm = utils.assignAvgofNeighbors(points=inputs.F, prm=prm, k=10)
-            #print("center and argmax center point ", points[np.argmax(prm)], points[valid_peak_list[idx][2]])
-            #peak_response_maps.append(prm)
-            valid_peak_response_map.append(prm)
-            peak_response_maps_con +=prm
-            #valid_peak_list contains indexes of valid peaks in center response map, shape: Mx3, e.g.[0,0,idx]
-            #valid_peak_list.append(valid_peak_list[idx,:])
+        grad = inputs.F.grad # N x input_channel_num
 
-    #if len(peak_response_maps) >0:
-    if False:
-    #if len(valid_peak_response_map) >0:
-        # shape = len(valid_peak_list), 2
-        valid_peak_list = np.vstack(valid_peak_list) # [1,1,N] -> dimension of each channels of it
-        #valid_peak_response_map = [peak_response_maps[i] for i in valid_indexes]
-        """
-        # Further point sampling 
-        # peak_centers: 3D info of subsampled peaks, peak_list: subsampled peak_list
-        points = np.asarray(inputs.F[:,0:3].detach().cpu())
-        peak_centers, valid_peak_list, valid_indexes = utils.FPS(valid_peak_list, points, num_frags=-1)
-        valid_peak_response_map = [peak_response_maps[i] for i in valid_indexes]
-        """
-        
-        # peak responses of each valid peak list is concatanated vertically
-        # shape = (len(valid_peak_list) * number_of_points_in_scene), channel_no_of_grad
-        #peak_response_maps_con = torch.cat(peak_response_maps, 0)
-        #peak_response_maps_con = sum(peak_response_maps)
-        #print("# of peak responses and shape ", len(peak_response_maps), valid_peak_list.shape, peak_response_maps[0].shape)
-    
-    
+        # PRM is absolute of all channels
+        prm = grad.detach().cpu().clone()
+        prm = np.asarray(np.absolute( prm )) # shape: N x input_channel_num, 2D
+        #normalize gradient 0 <= prm <= 1
+        if normalize:
+            prm = utils.maxpool(prm) #channel no is 1 from now on
+            #mins= np.amin(np.array(prm[prm>0.0]), axis=0)
+            #print(prm[prm>0.0].shape)
+            mins = np.asarray( [ np.amin(prm[prm[:,i]>0.0][:,i]) for i in range(prm.shape[1]) ] )
+            maxs = np.amax(np.array(prm), axis=0)
+            prm = (prm-mins)/(maxs-mins)
+            #print("min max ", mins, maxs)
+            prm[prm==float('inf')] = 0.0
+            prm[prm==float('-inf')] = 0.0
+            avg_sum += np.mean(prm[prm>0.0])
+            prm[prm<0.001] = 0.0
+
+            #prm = utils.assignAvgofNeighbors(points=inputs.F, prm=prm, k=10)
+        #print("center and argmax center point ", points[np.argmax(prm)], points[valid_peak_list[idx][2]])
+        #peak_response_maps.append(prm)
+        valid_peak_response_map.append(prm)
+        peak_response_maps_con +=prm
+        #valid_peak_list contains indexes of valid peaks in center response map, shape: Mx3, e.g.[0,0,idx]
+        #valid_peak_list.append(valid_peak_list[idx,:])
+
+
     return valid_peak_list, valid_peak_response_map, peak_response_maps_con, avg_sum
 
