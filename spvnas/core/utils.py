278395cfa12aaeff6ae8834a48f3aec6624d2eab
ezgic <ezgicakir96@hotmail.com>
2021-09-18 15:49:06 +0300

    nms_cpu version integrated.

--- spvnas/core/utils.py
+++ spvnas/core/utils.py
@@ -417,8 +417,41 @@ def non_maximum_supression(points, crm, peak_list, peak_responses,calibs):
         score = bbox[-1]
         dets[i] = np.asarray([[x1,y1,x2,y2,score]])
 
-    kept_idxs = nms_gpu(dets, nms_overlap_thresh=0.7, device_id=0)
+
+    #kept_idxs = nms_gpu(dets, nms_overlap_thresh=0.7, device_id=0)
+    kept_idxs = nms_cpu(dets, thresh=0.7)
     return kept_idxs
+
+def nms_cpu(dets, thresh, eps=0.0):
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+    scores = dets[:, 4]
+    areas = (x2 - x1 + eps) * (y2 - y1 + eps)
+    order = scores.argsort()[::-1].astype(np.int32)  # highest->lowest
+    ndets = dets.shape[0]
+    suppressed = np.zeros((ndets), dtype=np.int32)
+    keep = []
+    for _i in range(ndets):
+        i = order[_i]  # start with highest score box
+        if suppressed[
+            i] == 1:  # if any box have enough iou with this, remove it
+            continue
+        keep.append(i)
+        for _j in range(_i + 1, ndets):
+            j = order[_j]
+            if suppressed[j] == 1:
+                continue
+            # calculate iou between i and j box
+            w = max(min(x2[i], x2[j]) - max(x1[i], x1[j]) + eps, 0.0)
+            h = max(min(y2[i], y2[j]) - max(y1[i], y1[j]) + eps, 0.0)
+            inter = w * h
+            ovr = inter / (areas[i] + areas[j] - inter)
+            # ovr = inter / areas[j]
+            if ovr >= thresh:
+                suppressed[j] = 1
+    return keep
 
 def save_in_kitti_format(file_id, kitti_output, points, crm, peak_list, peak_responses, calibs, labels):
 
