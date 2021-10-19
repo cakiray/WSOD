6fa9d15158b27f1cbfd2f2a960d83c5182aeef40
ezgic <ezgicakir96@hotmail.com>
2021-10-19 09:08:41 +0300

    some printing

--- spvnas/core/models/semantic_kitti/spvnas_cnn.py
+++ spvnas/core/models/semantic_kitti/spvnas_cnn.py
@@ -326,6 +326,8 @@ class SPVNAS_CNN(nn.Module):
         z1.F = z1.F + self.point_transforms[0](z0.F)
 
         y1 = point_to_voxel(x4, z1)
+        print("\nx4",x4.F.shape)
+        print("\nz1",z1.F.shape)
         print("\ny1",y1.F.shape)
         #y1.F = self.dropout(y1.F)
         y1 = self.up1[0](y1)
